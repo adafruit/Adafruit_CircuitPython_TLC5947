c80bac578211c0ae87749b420ef7f01f4184525d
FoamyGuy <foamyguy@gmail.com>
2020-08-22 10:45:21 -0500

    use existing spi baudrate instead of hardcoded

--- adafruit_tlc5947.py
+++ adafruit_tlc5947.py
@@ -158,7 +158,7 @@ class TLC5947:
             # Lock the SPI bus and configure it for the shift register.
             while not self._spi.try_lock():
                 pass
-            self._spi.configure(baudrate=1000000, polarity=0, phase=0, bits=8)
+            self._spi.configure(baudrate=self._spi.frequency, polarity=0, phase=0, bits=8)
             # First ensure latch is low.
             self._latch.value = False
             # Write out the bits.
