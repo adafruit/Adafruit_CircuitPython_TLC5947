2977266e2eb14d9831aded83d11e0419b30a3eda
dherrada <dylan.herrada@adafruit.com>
2022-01-24 16:46:17 -0500

    Updated docs link, updated python docs link, updated setup.py

--- setup.py
+++ setup.py
@@ -45,8 +45,6 @@ setup(
         "Topic :: System :: Hardware",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
     ],
     # What does your project relate to?
     keywords="adafruit tlc5947 12-bit 24-channel led pwm hardware micropython circuitpython",
